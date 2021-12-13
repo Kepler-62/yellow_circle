d1a886ccd5292111f08f14e1c50dc0845cf21a75
Kepler-62 <94966677+Kepler-62@users.noreply.github.com>
2021-12-13 16:30:33 +0600

    Update draw.py

--- draw.py
+++ draw.py
@@ -3,14 +3,17 @@ import sys
 from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
+from ui import Ui_MainWindow
 
 import random
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)  # Загружаем дизайн
+        # Вызываем метод для загрузки интерфейса из класса Ui_MainWindow,
+        # остальное без изменений
+        self.setupUi(self)
         self.flag = False
         self.painter = QPainter()
         self.pushButton.clicked.connect(self.drawf)
@@ -23,7 +26,7 @@ class MyWidget(QMainWindow):
             self.flag = False
 
     def draw(self):
-        self.painter.setBrush(QColor('yellow'))
+        self.painter.setBrush(QColor(random.choice(range(256)), random.choice(range(256)), random.choice(range(256))))
         size = random.randint(10, 400)
         x0, y0 = 250, 200
         self.painter.drawEllipse(x0 - size // 2, y0 - size // 2, size, size)
